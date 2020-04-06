1c9760a8eb21b3389572baa6d27fb27530163b9a
Gabe <wei.g.chu@gmail.com>
2020-04-06 17:16:43 +0800

    remove registry

--- pii_recognition/tokenisation/tokenisers.py
+++ pii_recognition/tokenisation/tokenisers.py
@@ -13,15 +13,3 @@ def nltk_word_tokenizer(text: str) -> List[Token]:
         Token(text=text[span[0] : span[1]], start=span[0], end=span[1])
         for span in spans
     ]
-
-
-class TokeniserRegistry:
-    def __init__(self):
-        self.registry = {}
-        self.add_predefined_tokenisers()
-
-    def add_predefined_tokenisers(self):
-        self.add_tokeniser(nltk_word_tokenizer)
-
-    def add_tokeniser(self, tokeniser: Callable):
-        self.registry[tokeniser.__name__] = tokeniser
