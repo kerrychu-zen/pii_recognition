5ae95942f6e94fc7f08d6fa4b160ec5762c0f7da
Gabe <wei.g.chu@gmail.com>
2020-04-07 15:31:38 +0800

    add test for get recogniser

---
+++ pii_recognition/evaluation/pipeline_test.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+from pytest import raises
+
+from .pipeline import get_recogniser
+
+
+@patch(
+    "pii_recognition.evaluation.pipeline.recogniser_registry",
+    new={"fake_model": "fake_value"},
+)
+def test_get_recogniser():
+    actual = get_recogniser("fake_model")
+    assert actual == "fake_value"
+
+    with raises(KeyError) as err:
+        get_recogniser("non_exist_model")
+    assert str(err.value) == "'non_exist_model'"
