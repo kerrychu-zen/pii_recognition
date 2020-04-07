8c0dc3cc15b6fb64495b1696b2418344a48f846c
Gabe <wei.g.chu@gmail.com>
2020-04-07 15:30:41 +0800

    add recogniser registry

--- pii_recognition/registers/__init__.py
+++ pii_recognition/registers/__init__.py
@@ -1,3 +1,5 @@
+from .recogniser_registry import RecogniserRegistry
 from .tokeniser_registry import TokeniserRegistry
 
+recogniser_registry = RecogniserRegistry()
 tokeniser_registry = TokeniserRegistry()
