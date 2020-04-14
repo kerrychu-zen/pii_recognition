d483ca07f501656682aad1fc98b3366bc809a8c0
Gabe <wei.g.chu@gmail.com>
2020-04-14 11:55:03 +0800

    remove redundant test

--- pii_recognition/registers/tokeniser_registry_test.py
+++ pii_recognition/registers/tokeniser_registry_test.py
@@ -5,4 +5,3 @@ def test_TokeniserRegistry():
     actual = TokeniserRegistry()
     assert isinstance(actual, dict)
     assert len(actual.keys()) > 0
-    assert len(actual.keys()) == len(actual.values())
