9b06197f0287e0130672774aa800144f01bec96e
Gabe <wei.g.chu@gmail.com>
2020-04-16 00:10:51 +0800

    deactive tokeniser registry

--- pii_recognition/registers/tokeniser_registry.py
+++ pii_recognition/registers/tokeniser_registry.py
@@ -5,4 +5,6 @@ from .registry import Registry
 
 class TokeniserRegistry(Registry):
     def add_predefines(self):
-        self.add_item(nltk_word_tokenizer)
+        ...
+        # This class will be updated in the coming PR
+        # self.add_item(nltk_word_tokenizer)
