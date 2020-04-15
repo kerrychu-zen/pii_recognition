2826ffadb1371777d286c6f4e3ae4faaec9b7f96
Gabe <wei.g.chu@gmail.com>
2020-04-16 00:14:32 +0800

    replace with T_co

--- pii_recognition/registers/registry.py
+++ pii_recognition/registers/registry.py
@@ -1,10 +1,10 @@
 from abc import ABCMeta, abstractmethod
 from typing import Generic, Optional, Type, TypeVar
 
-T = TypeVar("T", covariant=True)
+T_co = TypeVar("T_co", covariant=True)
 
 
-class Registry(dict, Generic[T], metaclass=ABCMeta):
+class Registry(dict, Generic[T_co], metaclass=ABCMeta):
     def __init__(self):
         self.add_predefines()
 
@@ -13,11 +13,11 @@ class Registry(dict, Generic[T], metaclass=ABCMeta):
         """Use add_item method to put objects to registry."""
         ...
 
-    def add_item(self, item: Type[T], name: Optional[str] = None):
+    def add_item(self, item: Type[T_co], name: Optional[str] = None):
         if name:
             self[name] = item
         else:
             self[getattr(item, "__name__")] = item
 
-    def create_instance(self, name: str, **config) -> T:
+    def create_instance(self, name: str, **config) -> T_co:
         return self[name](**config)
