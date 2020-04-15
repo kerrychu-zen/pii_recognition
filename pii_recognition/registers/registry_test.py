cb609501a277650f6d5496e836a9ed85396832d9
Gabe <wei.g.chu@gmail.com>
2020-04-15 10:32:06 +0800

    add test for create_instance

--- pii_recognition/registers/registry_test.py
+++ pii_recognition/registers/registry_test.py
@@ -6,7 +6,7 @@ from .registry import Registry
 
 
 @patch.object(Registry, attribute="__abstractmethods__", new=set())
-def test_Registry():
+def test_Registry_add_item():
     def toy_func():
         pass
 
@@ -25,7 +25,20 @@ def test_Registry():
         actual.add_item(toy_func)
     assert str(err.value) == "The registered item must be a class object."
 
-
     with raises(TypeError) as err:
         actual.add_item(toy_assignment)
     assert str(err.value) == "The registered item must be a class object."
+
+
+@patch.object(Registry, attribute="__abstractmethods__", new=set())
+def test_Registry_create_instance():
+    class ToyClass:
+        def __init__(self, a):
+            self.a = a
+
+    registry = Registry()  # type: ignore
+    registry.add_item(ToyClass)
+
+    actual = registry.create_instance(name="ToyClass", a="a")
+    assert isinstance(actual, ToyClass)
+    assert actual.a == "a"
