64773163c2da982eec953a49e9a1409d3325ef10
Gabe <wei.g.chu@gmail.com>
2020-04-16 10:54:00 +0800

    use generic class no long need those files

--- pii_recognition/registers/tokeniser_registry.py
+++
@@ -1,8 +0,0 @@
-from .registry import Registry
-
-
-class TokeniserRegistry(Registry):
-    def add_predefines(self):
-        ...
-        # This class will be updated in the coming PR
-        # self.add_item(nltk_word_tokenizer)
