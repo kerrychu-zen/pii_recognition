ae90177c68dbceb35340595b30417f097f34f49c
Gabe <wei.g.chu@gmail.com>
2020-04-16 10:18:35 +0800

    remove add_predefines

--- pii_recognition/registers/registry.py
+++ pii_recognition/registers/registry.py
@@ -8,11 +8,6 @@ class Registry(dict, Generic[T_co], metaclass=ABCMeta):
     def __init__(self):
         self.add_predefines()
 
-    @abstractmethod
-    def add_predefines(self):
-        """Use add_item method to put objects to registry."""
-        ...
-
     def add_item(self, item: Type[T_co], name: Optional[str] = None):
         if name:
             self[name] = item
