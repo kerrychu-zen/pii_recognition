a1b8820b46ee1fce57a0775147950411fe04b041
Gabe <wei.g.chu@gmail.com>
2020-04-17 22:39:29 +0800

    black format

--- pii_recognition/recognisers/__init__.py
+++ pii_recognition/recognisers/__init__.py
@@ -1,2 +1,7 @@
-from . import (crf_recogniser, first_letter_uppercase_recogniser,  # noqa: F401
-               flair_recogniser, spacy_recogniser, stanza_recogniser)
+from . import (  # noqa: F401
+    crf_recogniser,
+    first_letter_uppercase_recogniser,
+    flair_recogniser,
+    spacy_recogniser,
+    stanza_recogniser,
+)
