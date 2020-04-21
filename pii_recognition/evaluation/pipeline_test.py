0f2780b23704d2176165d8dc74e324ad1a8a66e7
Gabe <wei.g.chu@gmail.com>
2020-04-21 10:27:54 +0800

    rename classes

--- pii_recognition/evaluation/pipeline_test.py
+++ pii_recognition/evaluation/pipeline_test.py
@@ -3,24 +3,29 @@ from unittest.mock import patch
 from .pipeline import get_recogniser
 
 
-class Simple:
+class RegistryNoConfig:
+    # class can be instantiated without passing any args
     pass
 
 
-class Complex:
-    def __init__(self, a):
-        self.a = a
+class RegistryWithConfig:
+    # must pass args to instantiate the class
+    def __init__(self, config_a):
+        self.config_a = config_a
 
 
 def mock_registry():
-    return {"Simple": Simple, "Complex": Complex}
+    return {
+        "RegistryNoConfig": RegistryNoConfig,
+        "RegistryWithConfig": RegistryWithConfig,
+    }
 
 
 @patch("pii_recognition.evaluation.pipeline.recogniser_registry", new=mock_registry())
 def test_get_recogniser():
-    actual = get_recogniser("Simple")["recogniser"]
-    assert isinstance(actual, Simple)
+    actual = get_recogniser("RegistryNoConfig")["recogniser"]
+    assert isinstance(actual, RegistryNoConfig)
 
-    actual = get_recogniser("Complex", {"a": "attr_a"})["recogniser"]
-    assert isinstance(actual, Complex)
-    assert actual.a == "attr_a"
+    actual = get_recogniser("RegistryWithConfig", {"config_a": "attr_a"})["recogniser"]
+    assert isinstance(actual, RegistryWithConfig)
+    assert actual.config_a == "attr_a"
