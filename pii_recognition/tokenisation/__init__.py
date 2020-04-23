c287ce76f81fbf675e5885b30b9bf706d3ec18c7
Gabe <wei.g.chu@gmail.com>
2020-04-23 19:12:31 +0800

    rename func

--- pii_recognition/tokenisation/__init__.py
+++ pii_recognition/tokenisation/__init__.py
@@ -2,11 +2,11 @@ from pii_recognition.registration.registry import Registry
 from pii_recognition.tokenisation.tokenisers import Tokeniser, TreebankWordTokeniser
 
 
-def init():
+def tokeniser_init():
     registry = Registry[Tokeniser]()
     registry.add_item(TreebankWordTokeniser)
 
     return registry
 
 
-tokeniser_registry: Registry[TreebankWordTokeniser] = init()
+tokeniser_registry: Registry[TreebankWordTokeniser] = tokeniser_init()
