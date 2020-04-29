0738944717e284568fc0bfa3f790662456d9ca4b
Gabe <wei.g.chu@gmail.com>
2020-04-29 15:13:11 +0800

    remove legacy class

--- pii_recognition/tokenisation/detokenisers.py
+++ pii_recognition/tokenisation/detokenisers.py
@@ -16,16 +16,3 @@ def space_join_detokensier(tokens: List[str]) -> str:
 
 def treebank_detokeniser(tokens: List[str]) -> str:
     return TreebankWordDetokenizer().detokenize(tokens)
-
-
-class DetokeniserRegistry:
-    def __init__(self):
-        self.registry = {}
-        self.add_predefined_detokenisers()
-
-    def add_predefined_detokenisers(self):
-        self.add_detokeniser(space_join_detokensier)
-        self.add_detokeniser(treebank_detokeniser)
-
-    def add_detokeniser(self, detokeniser: Callable):
-        self.registry[detokeniser.__name__] = detokeniser
