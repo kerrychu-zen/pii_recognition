e091f2c519c8b3c7d8dc205ae0f25d9107792045
Gabe <wei.g.chu@gmail.com>
2020-04-30 14:29:19 +0800

    update doc

--- pii_recognition/data_readers/reader.py
+++ pii_recognition/data_readers/reader.py
@@ -9,7 +9,7 @@ class Reader(metaclass=ABCMeta):
     @abstractmethod
     def get_test_data(self, file_path: str) -> Tuple[List[str], List[List[str]]]:
         """
-        Read test data and split into features and labels, where features are consumed
-        by a model to get predictions and labels are the ground truths.
+        Read test data and split into features and labels, where features are inputs
+        to a model and labels are the ground truths.
         """
         ...
