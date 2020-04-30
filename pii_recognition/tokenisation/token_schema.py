564afe1215c62917b2ad579acdef293476ce7bd2
Gabe <wei.g.chu@gmail.com>
2020-04-30 23:22:09 +0800

    add comment

--- pii_recognition/tokenisation/token_schema.py
+++ pii_recognition/tokenisation/token_schema.py
@@ -3,5 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Token:
+    # text is not included on purpose, otherwise it would make
+    # conversion between SpanLabel and TokenLabel difficult
     start: int
     end: int
