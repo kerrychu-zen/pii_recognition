f308b26bfbc350ad5c178d2fd26418f97fdc9a6e
Gabe <wei.g.chu@gmail.com>
2020-04-30 17:31:31 +0800

    add TokenLabel class

--- pii_recognition/labels/schema.py
+++ pii_recognition/labels/schema.py
@@ -1,5 +1,9 @@
-from dataclasses import dataclass
+from __future__ import annotations  # class forward reference
+
+from dataclasses import asdict, dataclass
 from typing import NamedTuple
+
+from pii_recognition.tokenisation.token_schema import Token
 
 
 @dataclass
@@ -7,6 +11,15 @@ class SpanLabel:
     entity_type: str
     start: int
     end: int
+
+
+@dataclass
+class TokenLabel(Token):
+    entity_type: str
+
+    @classmethod
+    def from_instance(cls, instance: Token, entity_type: str) -> TokenLabel:
+        return cls(entity_type=entity_type, **asdict(instance))
 
 
 class EvalLabel(NamedTuple):
