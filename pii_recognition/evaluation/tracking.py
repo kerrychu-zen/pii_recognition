d31fa0c4bcaaa5408d26bf3222ed764a2b3a0e98
Gabe <wei.g.chu@gmail.com>
2020-05-06 08:31:28 +0800

    add inline comments

--- pii_recognition/evaluation/tracking.py
+++ pii_recognition/evaluation/tracking.py
@@ -21,13 +21,18 @@ def start_tracker(
     run_name: str = "default",
     tracker_uri: str = DEFAULT_TRACKER_URI,
 ):
+    # Connect to a tracking URI.
+    # URI can either be a HTTP/HTTPS URI for a remote server, a database
+    # connection string, or a local path to log data to a directory.
     mlflow.set_tracking_uri(tracker_uri)
 
-    # create experiments at tracker_uri
+    # Set an experiment active, this experiment will be created under the
+    # tracking URI.
     mlflow.set_experiment(experiment_name)
 
-    experiment_id = _get_experiment_id(experiment_name)
-    mlflow.start_run(run_name=run_name, experiment_id=experiment_id)
+    # Start a new run and set it active, where the new run will be launched
+    # under the active experiment.
+    mlflow.start_run(run_name=run_name)
 
 
 def end_tracker():
