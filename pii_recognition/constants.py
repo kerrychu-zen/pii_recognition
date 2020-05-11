109d64f57fe90c7025bfaa2a70e17b07430a04a0
Gabe <wei.g.chu@gmail.com>
2020-05-11 17:30:05 +0800

    remove useless consts

--- pii_recognition/constants.py
+++ pii_recognition/constants.py
@@ -2,13 +2,3 @@ import os
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-# Dataset names
-CONLL = "conll"
-WNUT = "wnut"
-
-
-# reduce label discrepancies in evaluation metrics when
-# running on distinct datasets, e.g., CONLL is using "I-PER" to
-# reference person but in WNUT the label is "I-person"
-LABEL_COMPLIANCE = {"I-PER": "PERSON", "I-person": "PERSON"}
