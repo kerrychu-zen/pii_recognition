7bc8a536e9185f60480ca82fed7aafdb58b7b143
Gabe <wei.g.chu@gmail.com>
2020-07-08 08:55:38 +0800

    forward ticket id

---
+++ app_remote/flaskr/static/js/piiAppClient.js
@@ -0,0 +1,18 @@
+class PiiAppClient {
+    constructor(host) {
+        this.host = host;
+    }
+
+    replaceTicketId(payload) {
+        jsonifyBody = JSON.stringify(payload['ticket.id']);
+
+        return fetch(`${this.host}/update-ticket-id`, {
+            method: "PUT",
+            body: jsonifyBody,
+            cache: "no-cache",
+            headers: {'Content-Type': 'application/json'},
+        });
+    }        
+}
+
+export {PiiAppClient};
