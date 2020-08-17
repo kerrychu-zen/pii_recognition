f9f7ade4f03cf754010e2faf95f9ee74b30f49a7
Gabe <wei.g.chu@gmail.com>
2020-08-17 18:26:51 +0800

    rename text to entity

--- frontend/assets/components/redact.js
+++ frontend/assets/components/redact.js
@@ -4,15 +4,15 @@ import { entitiesStringSplitDelimiter } from "../constants.js";
 import client from "../apis/zafClient.js";
 
 const redact = async (text) => {
+  const entities = text.split(entitiesStringSplitDelimiter).map(removeWhitespace);
+
   const ticketId = await getTicketId();
   const comments = await getComments();
 
-  const texts = text.split(entitiesStringSplitDelimiter).map(removeWhitespace);
-
   for (const comment of comments) {
-    for (const text of texts) {
-      if (comment["text"].includes(text)) {
-        await requestRedactApi(ticketId, comment["id"], text);
+    for (const entity of entities) {
+      if (comment["text"].includes(entity)) {
+        await requestRedactApi(ticketId, comment["id"], entity);
       }
     }
   }
