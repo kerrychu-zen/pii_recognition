a20c0580fa4e9380a217c9a5186837177726c76a
Gabe <wei.g.chu@gmail.com>
2020-08-19 17:20:24 +0800

    map dropdown string to model vars

--- frontend/assets/components/piiDetect.js
+++ frontend/assets/components/piiDetect.js
@@ -1,31 +1,21 @@
 import { getCommentTexts } from "../apis/zafApis.js";
-import { detectPiiEntities as comprehendModel } from "../apis/comprehendApis.js";
 import { stripHtml, concatStrings } from "../utils.js";
-import {
-  commentsConcatDelimiter,
-  entitiesConcatDelimiter,
-} from "../constants.js";
+import { commentsConcatDelimiter } from "../constants.js";
+import models from "./dropdown.js";
 
 const textPreprocessor = (texts) => {
   const cleanTexts = texts.map((text) => stripHtml(text));
   return concatStrings(cleanTexts, commentsConcatDelimiter);
 };
 
-const predictPiiEntities = async (text, modelName) => {
-  // use return to show error to users
-  if (modelName === "Spacy") return new Error("Spacy model is not implemented");
-  else if (modelName === "Flair")
-    return new Error("Flair model is not implemented");
-  else if (modelName === "Comprehend") {
-    const entityArray = await comprehendModel(text);
-    return concatStrings(entityArray, entitiesConcatDelimiter);
-  }
+const predictPiiEntities = (text, modelName) => {
+  return models[modelName.toLowerCase()](text);
 };
 
 const getPiiEntities = async (modelName) => {
   const comments = await getCommentTexts();
-  const comment = textPreprocessor(comments);
-  const entities = await predictPiiEntities(comment, modelName);
+  const concatenatedComments = textPreprocessor(comments);
+  const entities = predictPiiEntities(concatenatedComments, modelName);
   return entities;
 };
 
