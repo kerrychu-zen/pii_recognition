0801074100329600a1810bebc2c987314a2c0228
Gabe <wei.g.chu@gmail.com>
2020-08-31 13:35:06 +0800

    separate aws ops from recogniser class

--- pii_recognition/recognisers/comprehend_recogniser.py
+++ pii_recognition/recognisers/comprehend_recogniser.py
@@ -1,14 +1,15 @@
 from typing import List
 
-import boto3
 from boto3.session import Session
 from botocore.client import BaseClient
 from decouple import config
 
+from pii_recognition.aws.config_session import config_cognito_session
 from pii_recognition.labels.schema import SpanLabel
 
 from .entity_recogniser import EntityRecogniser
 
+# read from .env
 IDENTITY_POOL_ID = config("IDENTITY_POOL_ID")
 AWS_REGION = "us-west-2"
 
@@ -16,42 +17,25 @@ AWS_REGION = "us-west-2"
 class ComprehendRecogniser(EntityRecogniser):
     def __init__(self, supported_entities: List[str],
                  supported_languages: List[str]):
-        sess = self._create_boto3_session(IDENTITY_POOL_ID)
+        sess = config_cognito_session(IDENTITY_POOL_ID, AWS_REGION)
         self.comprehend = self._initiate_comprehend(sess)
 
         super().__init__(supported_entities=supported_entities,
                          supported_languages=supported_languages)
-
-    def _create_boto3_session(self, identity_pool_id: str) -> Session:
-        client = boto3.client(service_name='cognito-identity',
-                              region_name="us-west-2")
-
-        response = client.get_id(IdentityPoolId=IDENTITY_POOL_ID)
-        identity_id = response["IdentityId"]
-
-        response = client.get_credentials_for_identity(IdentityId=identity_id)
-        secretKey = response['Credentials']['SecretKey']
-        accessKey = response['Credentials']['AccessKeyId']
-        sessionToken = response['Credentials']['SessionToken']
-
-        return Session(aws_access_key_id=accessKey,
-                       aws_secret_access_key=secretKey,
-                       aws_session_token=sessionToken,
-                       region_name=AWS_REGION)
 
     def _initiate_comprehend(self, session: Session) -> BaseClient:
         return session.client(service_name="comprehend",
                               region_name=AWS_REGION)
 
     def analyse(self, text: str, entities: List[str]) -> List[SpanLabel]:
-        # TODO: Add multilingual support but now only modeling on English.
+        # TODO: Add feature for multilingual support but the first round only English
         DEFAULT_LANG = 'en'
 
         response = self.comprehend.detect_entities(Text=text,
                                                    LanguageCode=DEFAULT_LANG)
         predicted_entities = response["Entities"]
 
-        # TODO: Consider add score to dataclass
+        # To be consistent with the other models, prediction scores are not added
         span_labels = list(
             map(
                 lambda entity: SpanLabel(entity["Type"], entity[
