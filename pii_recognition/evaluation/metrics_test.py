d6546a931cf7fc2a24268634df2babb22c16c63d
Gabe <wei.g.chu@gmail.com>
2020-09-23 12:19:47 +0800

    add test

--- pii_recognition/evaluation/metrics_test.py
+++ pii_recognition/evaluation/metrics_test.py
@@ -30,7 +30,7 @@ def test_compute_f_beta():
     assert np.isclose(actual, 0.44117647058)
 
 
-def test_compute_label_precision_for_list_binary():
+def test_compute_label_precision_for_binary():
     y_true = ["0", "0", "1"]
     y_pred = ["0", "1", "1"]
     actual = compute_label_precision(y_true, y_pred, label_name="1")
@@ -67,3 +67,10 @@ def test_compute_label_recall_for_multiclass():
     assert_almost_equal(actual, 0.3333333)
     actual = compute_label_recall(y_true, y_pred, label_name="2")
     assert_almost_equal(actual, 0.6666666)
+
+
+def test_compute_label_recall_for_nonexist_labels():
+    y_true = ["0", "1"]
+    y_pred = ["0", "1"]
+    actual = compute_label_recall(y_true, y_pred, label_name="2")
+    assert actual == 0.0
