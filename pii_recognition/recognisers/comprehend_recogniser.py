cf123952fa97f85f9c2effc8ad0378b1a1f1adc8
Gabe <wei.g.chu@gmail.com>
2020-10-08 17:21:09 +0800

    black format

--- pii_recognition/recognisers/comprehend_recogniser.py
+++ pii_recognition/recognisers/comprehend_recogniser.py
@@ -14,34 +14,33 @@ AWS_REGION = "us-west-2"
 
 
 class ComprehendRecogniser(EntityRecogniser):
-    def __init__(self, supported_entities: List[str],
-                 supported_languages: List[str]):
+    def __init__(self, supported_entities: List[str], supported_languages: List[str]):
         sess = config_cognito_session(IDENTITY_POOL_ID, AWS_REGION)
         self.comprehend = self._initiate_comprehend(sess)
 
-        super().__init__(supported_entities=supported_entities,
-                         supported_languages=supported_languages)
+        super().__init__(
+            supported_entities=supported_entities,
+            supported_languages=supported_languages,
+        )
 
     def _initiate_comprehend(self, session: Session) -> BaseClient:
-        return session.client(service_name="comprehend",
-                              region_name=AWS_REGION)
+        return session.client(service_name="comprehend", region_name=AWS_REGION)
 
     def analyse(self, text: str, entities: List[str]) -> Optional[List[Entity]]:
         self.validate_entities(entities)
 
         # TODO: Add multilingual support
-        DEFAULT_LANG = 'en'
+        DEFAULT_LANG = "en"
 
-        response = self.comprehend.detect_entities(Text=text,
-                                                   LanguageCode=DEFAULT_LANG)
+        response = self.comprehend.detect_entities(Text=text, LanguageCode=DEFAULT_LANG)
         predicted_entities = response["Entities"]
 
         # Enhancement: filter on comprehend prediction scores
-        filtered = filter(lambda ent: ent["Type"] in entities,
-                          predicted_entities)
+        filtered = filter(lambda ent: ent["Type"] in entities, predicted_entities)
         span_labels = map(
-            lambda ent: Entity(ent["Type"], ent["BeginOffset"], ent[
-                "EndOffset"]), filtered)
+            lambda ent: Entity(ent["Type"], ent["BeginOffset"], ent["EndOffset"]),
+            filtered,
+        )
 
         results = list(span_labels)
         if results:
